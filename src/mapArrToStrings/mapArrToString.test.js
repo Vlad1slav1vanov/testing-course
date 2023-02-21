3da92a6b4fb3737b9833514f18fca4e7fc96b6b7
Vladislav <typlomi@gmail.com>
2023-02-21 16:07:09 +0600

    clear mocks, before/after each

--- src/mapArrToStrings/mapArrToString.test.js
+++ src/mapArrToStrings/mapArrToString.test.js
@@ -1,1 +1,19 @@
 const mapArrToStrings = require('./mapArrToStrings');
+
+describe('mapArrToStrings', () => {
+  test('Корректное значение', () => {
+    expect(mapArrToStrings([1, 2, 3, 4])).toEqual(['1', '2', '3', '4']);
+  })
+
+  test('Разные типы данных', () => {
+    expect(mapArrToStrings([1, 2, 3, 4, null, undefined, '1234'])).toEqual(['1', '2', '3', '4']);
+  })
+
+  test('Пустой массив', () => {
+    expect(mapArrToStrings([])).toEqual([]);
+  })
+
+  test('Отрицание', () => {
+    expect(mapArrToStrings([1, 2, 3, 4])).not.toEqual(['1', '2', '3', '4', '5']);
+  })
+})
